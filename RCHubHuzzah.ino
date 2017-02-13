726a121122bb7665b234dbcaa985fd534140e13b
jens-o <Jens@iMac.local>
2017-02-13 21:36:11 +0100

    mqtt
    Publish sensors to MQTT broker
    Handle DHT sensor

--- RCHubHuzzah.ino
+++ RCHubHuzzah.ino
@@ -10,14 +10,38 @@
 #include <Hasta.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include "DHT.h"
+#include <PubSubClient.h>
+#include <stdlib.h>
 
 #include "wifiparams.h"
 
-int nbrCalls = 3;
+// The port to listen for incoming TCP connections
+#define LISTEN_PORT           80
+
+// The 433 TX pin
+#define TX_PIN                13
+
+// The DHT data pin
+#define DHTPIN 2 
+
+// The DHT data type
+//#define DHTTYPE DHT11 
+
+#define DALLAS
+
+// The default nbr of 433 calls
+#define DEFAULT_NBR_CALLS 5
+
+// Default interval reading sensor values
+#define DEFAULT_SENSOR_INTERVAL 30000 // 30s
 
 // Data wire is plugged into port on the Arduino
 #define ONE_WIRE_BUS 12
 
+int nbrCalls = DEFAULT_NBR_CALLS;
+
+#ifdef DALLAS
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
@@ -26,23 +50,31 @@ DallasTemperature sensors(&oneWire);
 
 // arrays to hold device address
 DeviceAddress insideThermometer;
-
-// The port to listen for incoming TCP connections
-#define LISTEN_PORT           80
-
-// The port to listen for incoming TCP connections
-#define TX_PIN                13
+#endif
 
 // Create an instance of the server
 WiFiServer server(LISTEN_PORT);
+WiFiClient client;
 
 Hasta hastaBlind = Hasta(TX_PIN);
+
+#ifdef DHTTYPE
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+#endif
+
+// Initialize Mqtt Client
+PubSubClient mqttClient(client);
+char msg[20];
+long lastMsg = 0;
+long sensorInterval = DEFAULT_SENSOR_INTERVAL;
 
 void setup(void)
 {
   // Start Serial
   Serial.begin(115200);
 
+#ifdef DALLAS
   Serial.println("Setup Temperature Sensor");
 
   // locate devices on the bus
@@ -71,6 +103,11 @@ void setup(void)
   Serial.print("Device 0 Resolution: ");
   Serial.print(sensors.getResolution(insideThermometer), DEC); 
   Serial.println();
+#endif
+
+#ifdef DHTTYPE
+  dht.begin();
+#endif
 
   pinMode(TX_PIN, OUTPUT);
 
@@ -93,6 +130,9 @@ void setup(void)
 
   // Start the server
   server.begin();
+
+  // Setup Mqtt Client
+  mqttClient.setServer("192.168.1.6", 1883);
 }
 
 void loop()
@@ -118,6 +158,41 @@ void loop()
  
     Serial.println("[Client disonnected]");
   }
+
+  
+
+  long now = millis();
+  if(now - lastMsg > sensorInterval) {
+    lastMsg = now;
+    publishSensors();
+    Serial.println("Published messages");
+  }
+}
+
+void publishSensors() {
+  float value;
+
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  mqttClient.loop();
+
+#ifdef DALLAS
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  value = sensors.getTempC(insideThermometer);
+  dtostrf(value,3,1,msg);
+  mqttClient.publish("huzzah1/exttemp", msg);
+#endif
+
+#ifdef DHTTYPE
+  value = dht.readTemperature();
+  dtostrf(value,3,1,msg);
+  mqttClient.publish("huzzah1/temp", msg);
+
+  value = dht.readHumidity();
+  dtostrf(value,3,1,msg);
+  mqttClient.publish("huzzah1/hum", msg);
+#endif
 }
 
 
@@ -126,27 +201,27 @@ String process(WiFiClient client) {
   // Jump over HTTP action
   client.readStringUntil('/');
 
+  // Just return if not an API call
+  if(client.readStringUntil('/') != "api") {
+    return String("HTTP/1.1 400 Bad Request\r\n") +
+            "Content-Type: text/html\r\n" +
+            "\r\n";
+  }
+
   // Get the command
   String command = client.readStringUntil('/');
 
-  if (command == "SwitchOn") {
-    switchOnOff(true, client);
+  if (command == "setswitch") {
+    setSwitch(client);
   }
-  else if (command == "SwitchOff") {
-    switchOnOff(false, client);
+  else if (command == "setblind") {
+    setBlind(client);
   }
-  else if (command == "BlindStop") {
-    blind(0, client);
+  /*
+  else if (command == "getweather") {
+    return getWeather(client);
   }
-  else if (command == "BlindDown") {
-    blind(1, client);
-  }
-  else if (command == "BlindUp") {
-    blind(2, client);
-  }
-  else if (command == "weather") {
-    return temperature(client);
-  }
+  */
   else {
     Serial.print("Received unknown command: ");
     Serial.println(command);
@@ -162,7 +237,12 @@ String process(WiFiClient client) {
 }
 
 // Custom function accessible by the API
-void switchOnOff(bool on, WiFiClient client) {
+void setSwitch(WiFiClient client) {
+  bool on = false;
+  
+  if (client.readStringUntil('/') == "on") {
+    on = true;
+  }
 
   // Get first param
   unsigned int device = client.parseInt();
@@ -183,10 +263,23 @@ void switchOnOff(bool on, WiFiClient client) {
   Serial.println(on);
 
   transmitter.sendUnit(device-1, on);
+
+/*
+  String msg = on ? "on" : "off";
+  String topic = "nexa/" + String(remoteId) + "/" + String(device);
+
+  char msgBuf[4];
+  msg.toCharArray(msgBuf, 4);
+
+  char topicBuf[20];
+  topic.toCharArray(topicBuf, 20);
+  
+  mqttClient.publish(topicBuf, msgBuf);
+*/
 }
 
-void blind(int command, WiFiClient client) {
-  
+void setBlind(WiFiClient client) {
+  String command = client.readStringUntil('/');
   int unit = client.parseInt();
   int house = client.parseInt();
 
@@ -201,32 +294,57 @@ void blind(int command, WiFiClient client) {
 
   for(int i=0; i<nbrCalls; i++)
   {
-    switch(command)
-    {
-      case 0: // stop
-        hastaBlind.stop(house, unit);
-        break;
-      case 1: // down
-        hastaBlind.down(house, unit);
-        break;
-      case 2: // up
-        hastaBlind.up(house, unit);
-        break;
+    if (command == "stop") {
+      hastaBlind.stop(house, unit);
+    }
+    else if (command == "down") {
+      hastaBlind.down(house, unit);
+    }
+    else if (command == "up") {
+      hastaBlind.up(house, unit);
     }
   }
 }
 
-String temperature(WiFiClient client)
+/*
+String getWeather(WiFiClient client)
 {
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-
   int unit = client.parseInt();
+
+  float t;
+  float h;
+
+  Serial.print("Requesting temperatures...");
   
-  float tempC = sensors.getTempC(insideThermometer);
-  Serial.print("Temp C: ");
-  Serial.println(tempC);
+  switch(unit) {
+    case 0: // Dallas      
+      sensors.requestTemperatures(); // Send the command to get temperatures
+      Serial.println("DONE");
+      t = sensors.getTempC(insideThermometer);
+      h = 0;
+      break;
+    case 1: // DHT
+      h = dht.readHumidity();
+      t = dht.readTemperature();
+
+      // Check if any reads failed and exit early (to try again).
+      if (isnan(h) || isnan(t)) {
+        Serial.println("Failed to read from DHT sensor!");
+
+        return String(
+                "HTTP/1.1 405 Internal Server Error\r\n"
+                "Content-Type: text/html\r\n"
+                "\r\n");
+      }
+      break;
+  } 
+    
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
   
   client.flush();
 
@@ -237,34 +355,17 @@ String temperature(WiFiClient client)
             "\r\n"
             "{\r\n"
             "\"temperature\": ") +
-         String(tempC) +
+         String(t) +
          String(
             ",\r\n"
             "\"humidity\": ") +
-         String(0) +
+         String(h) +
          String(
             "\r\n"
             "}\r\n"
             "\r\n");
-
-  /*
-  client.print(
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/json\r\n"
-            "Connection: close\r\n"
-            "\r\n");
-            
-  client.print("{\r\n");
-  client.print("\"temperature\": ");
-  client.print(tempC);
-  client.print(",\r\n");
-  client.print("\"humidity\": ");
-  client.print(0);
-  client.print("\r\n");
-  client.print("}\r\n");
-  client.print("\r\n");
-  */
 }
+*/
 
 // function to print a device address
 void printAddress(DeviceAddress deviceAddress)
@@ -273,5 +374,27 @@ void printAddress(DeviceAddress deviceAddress)
   {
     if (deviceAddress[i] < 16) Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+// Mqtt reconnect
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqttClient.publish("temp", "hello world");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
 }
